7e502b98221655799c18d57e4a8890f65bc3d4a6
= <eakosedag@gmail.com>
2025-11-02 21:49:38 +0300

    Basic image detection python file coded to be a helper for early tests

---
+++ oncomind-ml/tests/early_diagnosis_test_from_image.py
@@ -0,0 +1,37 @@
+from ultralytics import YOLO
+import argparse
+
+
+def main():
+     parser = argparse.ArgumentParser(
+          description="Run a script to test basic cancer cell classification model to help early diagnosis"
+     )
+     parser.add_argument(
+          "--model",
+          type=str,
+          required=True,
+          help="Path to the YOLO(or other) model file, e.g. yolov10n.pt",
+     )
+     parser.add_argument(
+          "--test-image",
+          type=str,
+          default=None,
+          help="Optional test image (path or URL) to verify inference with the chosen model.",
+     )
+
+     args = parser.parse_args()
+     
+     print(f"🔹 Loading YOLO model from {args.model} ...")
+     model = YOLO(args.model)
+     
+     if args.test_image:
+          print(f"🔹 Running inference on test image: {args.test_image}")
+          image = args.test_image
+          results = model(image)
+          results[0].show()
+          print("✅ Inference completed successfully!")
+
+if __name__ == "__main__":
+     main()
+
+
