23a88a922249ae5e57bf160ef942d79c4e7152b5
= <eakosedag@gmail.com>
2025-11-06 23:35:49 +0300

    scikit-learn libraries basic documentation codes have added they'll be revisited later

--- oncomind-ml/train_data.py
+++ oncomind-ml/train_data.py
@@ -1,3 +1,10 @@
 from sklearn.linear_model import LinearRegression
+from sklearn import datasets
 
 
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+print(digits.data)  
+
+digits.target
