f40ba59362c032e93220c5d094072f0d31607bef
= <eakosedag@gmail.com>
2025-11-28 20:03:24 +0300

    Admin Layout is added

---
+++ oncomind-frontend/src/layouts/AdminLayout..tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+const AdminLayout: React.FC = () => {
+
+     return (
+          <>
+               <Outlet />
+          </>
+     );
+};
+
+export default AdminLayout;
