75a3c519851d244ba52a51ebd6f21d659d8e0c74
= <eakosedag@gmail.com>
2025-12-06 22:44:52 +0300

    Font types are changed for a test

---
+++ oncomind-frontend/src/pages/dashboardpages/DashboardSettings.tsx
@@ -0,0 +1,32 @@
+// src/pages/LandingExact.tsx
+import React from "react";
+import "../css/Research.css";
+import "../css/index.css";
+
+export default function Research() {
+     return (
+
+          <>
+               <center className="research_container">
+                    <h1>Research</h1>
+                    <section>
+                         <h2 className="title_for_sections">How we accomlish our researches</h2>
+                         <p>
+                              fkdjflksdjlkfdjs
+                         </p>
+                    </section>
+                    <pre className="divider"></pre>
+                    <section>
+                         <h2 className="title_for_sections">How we accomlish our researches</h2>
+                         fdskfhjldksfjlksdj
+                    </section>
+                    <pre className="divider"></pre>
+                    <section>
+                         <h2 className="title_for_sections">How we accomlish our researches</h2>
+                         fgfldkjgkfldjl
+                    </section>
+               </center>
+          </>
+
+     )
+}
