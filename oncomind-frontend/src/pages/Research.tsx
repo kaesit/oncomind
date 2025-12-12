8039087132cf231ba5e2c0ddd4dc36ecb7629184
= <eakosedag@gmail.com>
2025-12-12 20:22:53 +0300

    Research page has a new update

--- oncomind-frontend/src/pages/Research.tsx
+++ oncomind-frontend/src/pages/Research.tsx
@@ -9,12 +9,21 @@ export default function Research() {
 
           <>
                <center className="research_container">
-                    <h1>Research</h1>
-                    <section>
-                         <h2 className="title_for_sections">How we accomlish our researches</h2>
-                         <p>
-                              fkdjflksdjlkfdjs
-                         </p>
+                    <h1 className="research-page-title">Research</h1>
+                    <section className="information-section">
+                         <div>
+                              <h2>Protoyping</h2>
+                              <img className="research-card-images" src="https://images.unsplash.com/photo-1557825835-70d97c4aa567?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bWVkaWNhbCUyMHRlY2h8ZW58MHx8MHx8fDA%3D" alt="" />
+                         </div>
+                         <div>
+                              <h2>Custom Datasets and Models</h2>
+                              <img className="research-card-images" src="https://images.unsplash.com/photo-1557825835-70d97c4aa567?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bWVkaWNhbCUyMHRlY2h8ZW58MHx8MHx8fDA%3D" alt="" />
+                         </div>
+                         <div>
+                              <h2>Real Bioinformatic Researches</h2>
+                              <img className="research-card-images" src="https://images.unsplash.com/photo-1557825835-70d97c4aa567?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bWVkaWNhbCUyMHRlY2h8ZW58MHx8MHx8fDA%3D" alt="" />
+
+                         </div>
                     </section>
                     <pre className="divider"></pre>
                     <section>
