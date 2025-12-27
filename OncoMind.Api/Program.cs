f3d8aceb7824d895114c47f998d78eead5d2cb18
= <eakosedag@gmail.com>
2025-12-27 16:16:52 +0300

    Docker is now working correctly!!!!!!!!!!!!!!!!!!!

--- OncoMind.Api/Program.cs
+++ OncoMind.Api/Program.cs
@@ -32,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseCors("AllowReactApp");
 app.UseAuthorization();
 app.MapControllers();
