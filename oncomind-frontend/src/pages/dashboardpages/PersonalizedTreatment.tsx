2e285a86b808c20d0c3a58e68a24438c921e7851
= <eakosedag@gmail.com>
2026-03-12 22:52:01 +0300

    chore: add new boxes for page

--- oncomind-frontend/src/pages/dashboardpages/PersonalizedTreatment.tsx
+++ oncomind-frontend/src/pages/dashboardpages/PersonalizedTreatment.tsx
@@ -8,6 +8,7 @@ import { SelectBox } from "devextreme-react/select-box";
 import { useNavigate } from "react-router-dom"; // For navigation
 import { patientService, PatientDto } from "../../services/patientService";
 import "../../css/DataSets.css";
+import "../../css/PersonalizedTreatment.css";
 import Form, { Item, Label, RequiredRule, RangeRule } from "devextreme-react/form";
 import notify from "devextreme/ui/notify"; // Nice toast notifications
 
@@ -22,6 +23,13 @@ const Patients: React.FC = () => {
             <h1>Personalized Treatment</h1>
             <p>This page is under construction</p>
             <p>this page will provide very first experimental features of future personalized treatment options</p>
+            <div className="pt_container">
+               <div className="pt_box"><h2>Drugs</h2></div>
+               <div className="pt_box"><h2>Patients</h2></div>
+               <div className="pt_box"><h2>Test</h2></div>
+            </div>
+            
+
           </div>
      );
 };
